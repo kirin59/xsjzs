22692ebf317f74c6910bf3dad89a05007cf4e908
kirin59 <menghuru3@qq.com>
2017-12-18 11:34:03 +0800

    init project info

---
+++ zsbs/src/main/java/com/xwj/xsj/zs/entity/StudentEntity.java
@@ -0,0 +1,28 @@
+package com.xwj.xsj.zs.entity;
+
+/**
+ * StudentEntity
+ *
+ * @author SunHaoTian
+ * @date 2017/12/18
+ */
+public class StudentEntity {
+    private String name;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
